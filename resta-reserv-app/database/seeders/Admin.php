35c3ec22d12e7098495c69cc4deb67641a2ec89d
kantm <kantm@DESKTOP-H26FR5O>
2023-05-18 11:43:54 +0200

    Add admin database seeder

--- resta-reserv-app/database/seeders/Admin.php
+++ resta-reserv-app/database/seeders/Admin.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class Admin extends Seeder
 {
@@ -12,6 +14,13 @@ class Admin extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'is_admin' => 1,
+        ]);
     }
 }
