13eacedaf8063ab649dc45de6fea0876c2da2ec6
kantm <kantm@DESKTOP-H26FR5O>
2023-05-22 14:05:41 +0200

    Add delete button to categories

--- resta-reserv-app/resources/views/admin/categories/index.blade.php
+++ resta-reserv-app/resources/views/admin/categories/index.blade.php
@@ -41,6 +41,20 @@
                                     <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {{ $category->description }}
                                     </td>
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <div class="flex space-x-2">
+                                            <a href="{{ route('admin.categories.edit', $category->id) }}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                                            <form
+                                                method="POST"
+                                                action="{{ route('admin.categories.destroy', $category->id) }}"
+                                                onsubmit="return confirm('Are you sure?');"
+                                                class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit">Delete</button>
+                                            </form>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
