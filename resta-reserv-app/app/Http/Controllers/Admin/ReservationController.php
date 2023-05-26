feda66ad9e636597b05d6678cbae1cd0839384e3
kantm <kantm@DESKTOP-H26FR5O>
2023-05-26 18:04:34 +0200

    Only show available tables when making a reservation

--- resta-reserv-app/app/Http/Controllers/Admin/ReservationController.php
+++ resta-reserv-app/app/Http/Controllers/Admin/ReservationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\TableStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReservationStoreRequest;
 use App\Models\Reservation;
@@ -25,7 +26,7 @@ class ReservationController extends Controller
      */
     public function create()
     {
-        $tables = Table::all();
+        $tables = Table::where('status', TableStatus::Available)->get();
         return view('admin.reservations.create', compact('tables'));
     }
 
