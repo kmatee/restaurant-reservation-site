84726ec90bbcc47e82ef91ef548c711a9a89be2c
kantm <kantm@DESKTOP-H26FR5O>
2023-05-31 14:43:46 +0200

    Add routes to menu links

--- resta-reserv-app/resources/views/layouts/guest.blade.php
+++ resta-reserv-app/resources/views/layouts/guest.blade.php
@@ -44,9 +44,9 @@
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
                         href="#">Categories</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        href="#">Our Menu</a>
+                        href="{{ route('menus.index') }}">Our Menu</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        href="#">Make Reservation</a>
+                        href="{{ route('reservation.step.one') }}">Make Reservation</a>
     
                 </div>
             </nav>
