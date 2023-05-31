d757565b554e4b2af85ba937f965d8c59f77b9f5
kantm <kantm@DESKTOP-H26FR5O>
2023-05-31 14:28:09 +0200

    Add menu

--- resta-reserv-app/resources/views/layouts/guest.blade.php
+++ resta-reserv-app/resources/views/layouts/guest.blade.php
@@ -15,16 +15,45 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+        <div class="bg-white shadow-md" x-data="{ isOpen: false }">
+            <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+                <div class="flex items-center justify-between">
+                    <a class="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-2xl hover:text-green-400"
+                        href="#">
+                        TailFood
+                    </a>
+                    <!-- Mobile menu button -->
+                    <div @click="isOpen = !isOpen" class="flex md:hidden">
+                        <button type="button"
+                            class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                            aria-label="toggle menu">
+                            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                                <path fill-rule="evenodd"
+                                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                                </path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+    
+                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                <div :class="isOpen ? 'flex' : 'hidden'"
+                    class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+                    <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                        href="/">Home</a>
+                    <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                        href="#">Categories</a>
+                    <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                        href="#">Our Menu</a>
+                    <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                        href="#">Make Reservation</a>
+    
+                </div>
+            </nav>
+        </div>
+        <div class="font-sans text-gray-900 antialiased min-h-screen">
+            {{ $slot }}
+        </div>
         </div>
         <footer class="bg-gray-800 border-t border-gray-200">
             <div class="container flex flex-wrap items-center justify-center px-4 py-8 mx-auto lg:justify-between">
