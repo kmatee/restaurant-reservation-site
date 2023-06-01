51283f18871fa348ba6a5a1dd4a6fede5d71c8e6
kantm <kantm@DESKTOP-H26FR5O>
2023-06-01 15:59:59 +0200

    Remove read more link

--- resta-reserv-app/resources/views/welcome.blade.php
+++ resta-reserv-app/resources/views/welcome.blade.php
@@ -36,17 +36,7 @@
                 culpa minus eveniet, id nesciunt excepturi sit voluptate repudiandae. Explicabo, incidunt quia.
                 Repellendus mollitia quaerat est voluptas!
               </p>
-              <div class="relative flex">
-                <a href="#_"
-                  class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-green-600 rounded-md sm:mb-0 hover:bg-green-700 sm:w-auto">
-                  Read More
-                  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                    <polyline points="12 5 19 12 12 19"></polyline>
-                  </svg>
-                </a>
-              </div>
+              
             </div>
           </div>
           <div class="w-full md:w-1/2">
