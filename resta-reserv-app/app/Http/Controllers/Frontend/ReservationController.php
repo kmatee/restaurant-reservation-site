07f47908123c07fc886617029db2310364ffdeb2
kantm <kantm@DESKTOP-H26FR5O>
2023-06-01 10:40:20 +0200

    Add stepOne and storeStepOne methods to ReservationController

--- resta-reserv-app/app/Http/Controllers/Frontend/ReservationController.php
+++ resta-reserv-app/app/Http/Controllers/Frontend/ReservationController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Rules\DateBetween;
+use App\Rules\TimeBetween;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function stepOne()
+    public function stepOne(Request $request)
     {
-        return 'step one';
+        $reservation = $request->session()->get('reservation');
+        $min_date = Carbon::today();
+        $max_date = Carbon::now()->addWeek();
+        return view('reservations.step-one', compact('reservation', 'min_date', 'max_date'));
+    }
+
+    public function storeStepOne(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'reservation_date' => ['required', 'date', new DateBetween, new TimeBetween],
+            'tel_number' => ['required'],
+            'table_id' => ['required'],
+            'guest_number' => ['required'],
+        ]);
     }
 }
