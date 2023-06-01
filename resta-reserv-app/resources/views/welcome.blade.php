aec27399256ca77362f384a229e29d84825f81a3
kantm <kantm@DESKTOP-H26FR5O>
2023-06-01 15:59:35 +0200

    Change welcome page title and gradient color

--- resta-reserv-app/resources/views/welcome.blade.php
+++ resta-reserv-app/resources/views/welcome.blade.php
@@ -4,12 +4,11 @@
       class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
       style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
       <h1
-        class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-center sm:leading-none lg:text-5xl">
-        <span class="inline md:block">Welcome To Larainfo Restaurant</span>
+        class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-green-600 md:text-center sm:leading-none lg:text-5xl">
+        <span class="inline md:block">Welcome To Gustavo Pizzeria webpage</span>
       </h1>
       <div class="mx-auto mt-2 text-green-50 md:text-center lg:text-lg">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta voluptatem ab necessitatibus illo praesentium
-        culpa excepturi quae commodi quaerat,
+        The best family owned Italian Resaturant in Budapest.
       </div>
       <div class="flex flex-col items-center mt-12 text-center">
         <span class="relative inline-flex w-full md:w-auto">
