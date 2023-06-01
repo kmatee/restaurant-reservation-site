ddca0f05ad267607ceadde5171c4ab5c58f0d0d4
kantm <kantm@DESKTOP-H26FR5O>
2023-06-01 15:36:31 +0200

    Fix: the chosen date stays filled with previous data

--- resta-reserv-app/resources/views/reservations/step-one.blade.php
+++ resta-reserv-app/resources/views/reservations/step-one.blade.php
@@ -75,7 +75,7 @@
                                         <input type="datetime-local" id="reservation_date" name="reservation_date"
                                             min="{{ $min_date->format('Y-m-d\TH:i:s') }}"
                                             max="{{ $max_date->format('Y-m-d\TH:i:s') }}"
-
+                                            value="{{ $reservation ? \Carbon\Carbon::parse($reservation->reservation_date)->format('Y-m-d\TH:i:s') : '' }}"
                                             class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     <span class="text-xs">Please choose the time between 14:00-22:00.</span>
