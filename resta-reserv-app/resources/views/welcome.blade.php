5212f98a84a660d291febc4cd163eef6916c6595
kantm <kantm@DESKTOP-H26FR5O>
2023-06-01 16:18:36 +0200

    Change reviews text

--- resta-reserv-app/resources/views/welcome.blade.php
+++ resta-reserv-app/resources/views/welcome.blade.php
@@ -76,8 +76,8 @@
     <section class="pt-4 pb-12 bg-gray-800">
       <div class="my-16 text-center">
         <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-          Testimonial </h2>
-        <p class="text-xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. soluta sapient</p>
+          Reviews </h2>
+        <p class="text-xl text-white">Some reviews from our dear customers</p>
       </div>
       <div class="grid gap-2 lg:grid-cols-3">
         <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
@@ -86,10 +86,8 @@
               src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80">
           </div>
           <div>
-            <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-              ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
-              minus consequuntur!</p>
+            <h2 class="text-3xl font-semibold text-gray-800">Pizza</h2>
+            <p class="mt-2 text-gray-600">I had one of the best pizza of my life at Gustavos. I can only recommend this place!</p>
           </div>
           <div class="flex justify-end mt-4">
             <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
@@ -101,13 +99,11 @@
               src="https://cdn.pixabay.com/photo/2018/01/04/21/15/young-3061652__340.jpg">
           </div>
           <div>
-            <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-              ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
-              minus consequuntur!</p>
+            <h2 class="text-3xl font-semibold text-gray-800">Pasta</h2>
+            <p class="mt-2 text-gray-600">The homemade pastas here are excellent. It's just like my grandma's!</p>
           </div>
           <div class="flex justify-end mt-4">
-            <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
+            <a href="#" class="text-xl font-medium text-green-500">Erica Wells</a>
           </div>
         </div>
         <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
@@ -116,13 +112,11 @@
               src="https://cdn.pixabay.com/photo/2018/01/18/17/48/purchase-3090818__340.jpg">
           </div>
           <div>
-            <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-              ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
-              minus consequuntur!</p>
+            <h2 class="text-3xl font-semibold text-gray-800">Atmosphere</h2>
+            <p class="mt-2 text-gray-600">The atmosphere is just amazing at Gustavos. Very cozy and warm. The service was excellent too!</p>
           </div>
           <div class="flex justify-end mt-4">
-            <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
+            <a href="#" class="text-xl font-medium text-green-500">Melinda Watson</a>
           </div>
         </div>
       </div>
