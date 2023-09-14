5731656dc8054a5952c22434555424f56397e2d5
kantm <kantm@DESKTOP-H26FR5O>
2023-09-14 19:28:56 +0200

    Wrap fields in a form

--- resta-reserv-app/resources/views/order-confirm.blade.php
+++ resta-reserv-app/resources/views/order-confirm.blade.php
@@ -32,59 +32,61 @@
      <div class="grid  gap-8 grid-cols-1">
         <div class="flex flex-col ">
            <div class="mt-5">
-              <form action="">
+              <form action="{{route('order-confirm')}}" method="POST">
+                @csrf
               <div class="form">
                  <div class="md:space-y-2 mb-3">
                     <div class="md:flex flex-row md:space-x-4 w-full text-xs">
                        <div class="mb-3 space-y-2 w-full text-xs">
                           <label class="font-semibold text-gray-600 py-2">First  Name <abbr title="required">*</abbr></label>
-                          <input placeholder="First Name" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
+                          <input placeholder="First Name" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" required="required" type="text" name="first_name" id="first_name">
                           <p class="text-red text-xs hidden">Please fill out this field.</p>
                        </div>
                        <div class="mb-3 space-y-2 w-full text-xs">
                           <label class="font-semibold text-gray-600 py-2">Last Name <abbr title="required">*</abbr></label>
-                          <input placeholder="Last Name" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" required="required" type="text" name="integration[shop_name]" id="integration_shop_name">
+                          <input placeholder="Last Name" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" required="required" type="text" name="last_name" id="last_name">
                           <p class="text-red text-xs hidden">Please fill out this field.</p>
                        </div>
                     </div>
                     <label class=" font-semibold text-gray-600 text-xs py-2">Phone number<abbr title="required">*</abbr></label>
                     <div class="flex flex-wrap items-stretch w-full mb-4 relative">
-                       <input type="tel" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border border-l-1 h-10 border-grey-light rounded-lg rounded-l-1 px-3 relative focus:border-blue focus:shadow" placeholder="(+36...)">
+                       <input type="tel" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border border-l-1 h-10 border-grey-light rounded-lg rounded-l-1 px-3 relative focus:border-blue focus:shadow" placeholder="(+36...)" name="phone_number">
                     </div>
                  </div>
                  <div class="mb-3 space-y-2 w-full text-xs">
                     <label class="font-semibold text-gray-600 py-2">Address<abbr title="required">*</abbr></label>
                     <div class="flex flex-wrap items-stretch w-full mb-4 relative">
-                       <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border border-l-1 h-10 border-grey-light rounded-lg rounded-l-1 px-3 relative focus:border-blue focus:shadow" placeholder="City, address">
+                       <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border border-l-1 h-10 border-grey-light rounded-lg rounded-l-1 px-3 relative focus:border-blue focus:shadow" placeholder="City, address" name="address">
                     </div>
                  </div>
                  <div class="md:flex md:flex-row md:space-x-4 w-full text-xs">
                     <div class="w-full flex flex-col mb-3">
                        <label class="font-semibold text-gray-600 py-2">ZIP Code<abbr title="required">*</abbr></label>
-                       <input placeholder="ZIP" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" type="number" name="integration[street_address]" id="integration_street_address">
+                       <input placeholder="ZIP" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" type="number" name="zip_code" id="zip_code">
                     </div>
                     <div class="w-full flex flex-col mb-3">
                        <label class="font-semibold text-gray-600 py-2">Country<abbr title="required">*</abbr></label>
-                       <select class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required="required" name="integration[city_id]" id="integration_city_id">
-                          <option value="">Hungary</option>
-                          <option value="">Germany</option>
-                          <option value="">France</option>
-                          <option value="">Netherlands</option>
+                       <select class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required="required" name="country" id="country">
+                          <option value="Hungary">Hungary</option>
+                          <option value="Germany">Germany</option>
+                          <option value="France">France</option>
+                          <option value="Netherlands">Netherlands</option>
                        </select>
                        <p class="text-sm text-red-500 hidden mt-3" id="error">Please fill out this field.</p>
                     </div>
                  </div>
                  <div class="flex-auto w-full mb-1 text-xs space-y-2">
                     <label class="font-semibold text-gray-600 py-2">Additional information for shipping</label>
-                    <textarea required="" name="message" id="" class="w-full min-h-[100px] max-h-[300px] h-28 appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg  py-4 px-4" placeholder="Enter extra information" spellcheck="false"></textarea>
+                    <textarea name="message" id="" class="w-full min-h-[100px] max-h-[300px] h-28 appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg  py-4 px-4" placeholder="Enter extra information" spellcheck="false"></textarea>
                  </div>
                  <p class="text-xs text-red-500 text-right my-3">Required fields are marked with an
                     asterisk <abbr title="Required field">*</abbr>
                  </p>
                  <div class="mt-5 text-right md:space-x-3 md:block flex flex-col-reverse">
-                    <button class="mb-2 md:mb-0 px-5 py-2 text-sm shadow-sm font-medium tracking-wider bg-blue-500 text-blue-50 hover:bg-blue-600 rounded-md">Confirm</button>
+                    <button type="submit" class="mb-2 md:mb-0 px-5 py-2 text-sm shadow-sm font-medium tracking-wider bg-blue-500 text-blue-50 hover:bg-blue-600 rounded-md">Confirm</button>
                  </div>
               </div>
+            </form>
            </div>
         </div>
      </div>
