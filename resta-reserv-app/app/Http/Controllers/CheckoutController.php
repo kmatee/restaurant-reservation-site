ea00ff4d1749322162a84017fd2db45116b4ee01
kantm <kantm@DESKTOP-H26FR5O>
2023-09-18 18:42:28 +0200

    Create store method to save order data to DB

--- resta-reserv-app/app/Http/Controllers/CheckoutController.php
+++ resta-reserv-app/app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -9,5 +10,22 @@ class CheckoutController extends Controller
     public function index()
     {
         return view('thankyou');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        Order::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'phone_number' => $request->input('phone_number'),
+            'address' => $request->input('address'),
+            'zip_code' => $request->input('zip_code'),
+            'country' => $request->input('country'),
+            'items' => $request->input('items'),
+            'total' => $request->input('total'),
+        ]);
+
+        return to_route('thankyou-order');
     }
 }
