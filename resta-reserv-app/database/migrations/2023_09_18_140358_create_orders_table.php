d0a4efc52a6660b6ba01f88d2869b05f3646e179
kantm <kantm@DESKTOP-H26FR5O>
2023-09-18 17:08:14 +0200

    Create migration file for Order model

---
+++ resta-reserv-app/database/migrations/2023_09_18_140358_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('phone_number');
+            $table->string('address');
+            $table->integer('zip_code');
+            $table->string('country');
+            $table->string('items');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
