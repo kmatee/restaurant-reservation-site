a2763bd240e785d8095fa12dbfe0068771920cc5
kantm <kantm@DESKTOP-H26FR5O>
2023-09-20 15:41:14 +0200

    Create table for displaying order items

--- resta-reserv-app/resources/views/admin/orders/items/index.blade.php
+++ resta-reserv-app/resources/views/admin/orders/items/index.blade.php
@@ -1,11 +1,57 @@
 
 
 <x-admin-layout>
-    @foreach ($items as $item)
-    <div class="item">
-        <p>Name: {{ $item['name'] }}</p>
-        <p>Price: {{ $item['price'] }}</p>
-        <!-- Other item details -->
-    </div>
-@endforeach
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <table class="w-1/2 text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Item Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Quantity
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($items as $item)
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$item['name']}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$item['price']}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$item['quantity']}}
+                </td>
+                <td class="px-6 py-4">
+                    <div class="flex space-x-2">
+                        <a href="" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                        <form
+                            method="POST"
+                            action=""
+                            onsubmit="return confirm('Are you sure?');"
+                            class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
 </x-admin-layout>
