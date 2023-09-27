77d0ee5ef719d6f62d996f26aaad1506b025ac76
kantm <kantm@DESKTOP-H26FR5O>
2023-09-27 17:26:39 +0200

    Add payment method implementing stripe payment

--- resta-reserv-app/app/Http/Controllers/PaymentController.php
+++ resta-reserv-app/app/Http/Controllers/PaymentController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+
+    public function getCartItems()
+    {
+        $user_id = auth()->user()->id;
+        \Cart::session($user_id);
+        $items = \Cart::getContent();
+
+        return $items;
+    }
+
+    public function payment()
+    {
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SK'));
+
+        $items = $this->getCartItems();
+        $lineItems = [];
+        foreach ($items as $item){
+            $lineItems[] = [
+                'price_data' => [
+                    'currency' => 'huf',
+                    'product_data' => [
+                    'name' => $item->name,
+                    ],
+                    'unit_amount' => $item->price * 100,
+                ],
+                'quantity' => $item->quantity,
+            ];
+        }
+
+        $checkout_session = $stripe->checkout->sessions->create([
+            'line_items' => $lineItems,
+            'mode' => 'payment',
+            'success_url' => 'http://localhost:4242/success',
+            'cancel_url' => 'http://localhost:4242/cancel',
+        ]);
+
+        return redirect($checkout_session->url);
+    }
 }
