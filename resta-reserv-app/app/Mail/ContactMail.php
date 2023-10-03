84ea1448051d9718b996d6184e3df87b5e9ea0ab
kantm <kantm@DESKTOP-H26FR5O>
2023-10-03 18:44:57 +0200

    Create ContactMail mailable object

---
+++ resta-reserv-app/app/Mail/ContactMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this->subject('Contact Form Submission')
+                    ->from($this->data['email'], $this->data['name'])
+                    ->view('contact-email');
+    }
+    
+}
