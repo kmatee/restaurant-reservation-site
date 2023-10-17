ff9ab4718fb7e23e8237ad44aa0b752282918a81
kantm <kantm@DESKTOP-H26FR5O>
2023-10-17 16:30:33 +0200

    Update thanyou view frontend design

--- resta-reserv-app/resources/views/thankyou.blade.php
+++ resta-reserv-app/resources/views/thankyou.blade.php
@@ -1,6 +1,9 @@
 <x-guest-layout>
-    <div class="container w-full px-5 py-6 mx-auto">
-        <h1>Thank you!</h1>
-        <p>Your reservation is booked</p>
-      </div>
+  <div class="static pt-12 text-center">
+    <div class="inline-block rounded-lg bg-gray-100 p-12">
+      <h2 class="text-xl font-bold">Thank you for your reservation!</h2>
+      <p class="">Your table is reserved for the chosen date and time.</p>
+      <p class="">You will recieve an email about your reservation details.</p>
+    </div>
+</div>
 </x-guest-layout>
