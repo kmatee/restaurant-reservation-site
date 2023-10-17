b697a92c2c2e078a970e7869f932ce9303809200
kantm <kantm@DESKTOP-H26FR5O>
2023-10-17 16:25:08 +0200

    Update thanyou-order view frontend design

--- resta-reserv-app/resources/views/thankyou-order.blade.php
+++ resta-reserv-app/resources/views/thankyou-order.blade.php
@@ -1,5 +1,9 @@
 <x-guest-layout>
-    <div class="text-center">
-        <h2 class="text-xl font-bold">Thank you for your order!</h2>     
+    <div class="static pt-12 text-center">
+        <div class="inline-block rounded-lg bg-gray-100 p-12">
+          <h2 class="text-xl font-bold">Thank you for your order!</h2>
+          <p class="">Our collegues started processing your order and will be delivered to you shortly!</p>
+          <p class="">You will recieve an email about your order details</p>
+        </div>
     </div>
 </x-guest-layout>
