bf29b04a35b6f1d2de49b9868fc3c271cec7b6ac
kantm <kantm@DESKTOP-H26FR5O>
2023-10-19 16:32:44 +0200

    Fix menu items alignment on small screens

--- resta-reserv-app/resources/views/menus/index.blade.php
+++ resta-reserv-app/resources/views/menus/index.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="container w-full px-5 py-6 mx-auto">
-        <div class="grid lg:grid-cols-4 gap-y-6">
+        <div class="grid lg:grid-cols-4 gap-y-6 place-items-center">
             @foreach ($menus as $menu )
             <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
               <img class="w-full h-48" src="{{ Storage::url($menu->image) }}"
