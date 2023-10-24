5b4521f69a58ab0689e78376958312cf99f059af
kantm <kantm@DESKTOP-H26FR5O>
2023-10-24 22:16:45 +0200

    Change register background to white

--- resta-reserv-app/resources/views/auth/register.blade.php
+++ resta-reserv-app/resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <section class="bg-gray-50 dark:bg-gray-100">
+    <section class="bg-white">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-600 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
