d3ba3fbfe63a16a9c90c01b031fe911c3f4fd7c7
kantm <kantm@DESKTOP-H26FR5O>
2023-10-24 22:16:06 +0200

    Change login background to white

--- resta-reserv-app/resources/views/auth/login.blade.php
+++ resta-reserv-app/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <section class="bg-gray-50 dark:bg-gray-100">
+    <section class="bg-white">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-600 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
