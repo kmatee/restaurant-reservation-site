4b96486468cf5923335b90aaa807d02f4dfd4f4d
kantm <kantm@DESKTOP-H26FR5O>
2023-10-24 16:53:45 +0200

    Center "Make Reservation" button on large screens

--- resta-reserv-app/resources/views/welcome.blade.php
+++ resta-reserv-app/resources/views/welcome.blade.php
@@ -1,21 +1,22 @@
 <x-guest-layout>
     <!-- Main Hero Content -->
     <div
-      class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
+      class="container max-w-lg px-4 py-32 items-center mx-auto text-center bg-center bg-no-repeat bg-cover md:max-w-none md:text-center sm:max-v-none sm:text-center"
       style="background-image: url('https://eatkc.com/wp-content/uploads/2019/01/Italian.jpg')">
       <h1
-        class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-white to-white md:text-center sm:leading-none lg:text-5xl">
+        class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-white to-white md:text-center sm:leading-none lg:text-4xl">
         <span class="inline md:block">Welcome To Gustavo Pizzeria webpage</span>
       </h1>
       <div class="mx-auto mt-2 text-white md:text-center lg:text-lg">
         The best family owned Italian Resaturant in Budapest.
       </div>
-      <div class="flex flex-col items-center mt-12 text-center">
-        <span class="relative inline-flex w-full md:w-auto">
+      <div class="grid place-items-center">
+        <div class="md:w-auto pt-12">
           <a href="{{ route('reservations.step.one') }}" type="button"
-            class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
+            class="px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
             Make Reservation
           </a>
+        </div>
       </div>
     </div>
     <!-- End Main Hero Content -->
