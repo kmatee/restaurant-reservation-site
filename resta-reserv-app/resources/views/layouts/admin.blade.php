28485d750b3c5a335851fe2ecb7218ad49cefe6c
kantm <kantm@DESKTOP-H26FR5O>
2023-10-31 19:28:42 +0100

    Change bg color of admin layout page

--- resta-reserv-app/resources/views/layouts/admin.blade.php
+++ resta-reserv-app/resources/views/layouts/admin.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-700">
         <div class="flex-col w-full md:flex md:flex-row md:min-h-screen">
         <div @click.away="open = false" class="flex flex-col flex-shrink-0 w-full text-gray-700 bg-slate-100 md:w-64 dark:text-gray-200 dark:bg-gray-800" x-data="{ open: false }">
             <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4">
