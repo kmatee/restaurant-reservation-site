632bffa088bfe8cea7fd5ebaf36d88aff8a834b3
kantm <kantm@DESKTOP-H26FR5O>
2023-10-31 19:29:04 +0100

    Add new mobile view of the categories table

--- resta-reserv-app/resources/views/admin/categories/index.blade.php
+++ resta-reserv-app/resources/views/admin/categories/index.blade.php
@@ -11,14 +11,14 @@
                 <a href="{{ route('admin.categories.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">New Category</a>
             </div>
             <div class="flex flex-col">
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg hidden sm:block">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-800 dark:text-gray-400">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="px-6 py-3 w-16">
                                     Name
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="px-6 py-3 sm:w-32">
                                     Image
                                 </th>
                                 <th scope="col" class="px-6 py-3">
@@ -29,14 +29,14 @@
                                 </th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="sm:flex-none">
                             @foreach ($categories as $category)
                                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                                     <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {{ $category->name }}
                                     </td>
-                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        <img src="{{ Storage::url($category->image) }}" class="w-16 h-16 rounded">
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white shrink-0">
+                                        <img src="{{ Storage::url($category->image) }}" class="w-16 h-16 rounded shrink-0">
                                     </td>
                                     <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         {{ $category->description }}
@@ -59,6 +59,40 @@
                             @endforeach
                         </tbody>
                     </table>
+                </div>
+                <!-- Mobile view table -->
+                <div class="grid grid-cols-1 gap-4 md:hidden sm:hidden">
+                    @foreach ($categories as $category)
+                    <div class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 rounded-lg shadow">
+                        <div class="space-x-3 text-xs text-gray-100 space-y-2">
+                            <div class="pl-3 pt-2 text-white uppercase text-sm">
+                                {{ $category->name }}
+                            </div>
+                            <div class="">
+                                <img src="{{ Storage::url($category->image) }}" class="w-16 h-16 rounded shrink-0">
+                            </div>
+                            <div class="flex justify-between">
+                                <div class="pr-12 bottom-0">
+                                    {{ $category->description }}
+                                </div>
+                                <div>
+                                    <div class="flex space-x-2 pr-2 pb-2">
+                                        <a href="{{ route('admin.categories.edit', $category->id) }}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                                        <form
+                                            method="POST"
+                                            action="{{ route('admin.categories.destroy', $category->id) }}"
+                                            onsubmit="return confirm('Are you sure?');"
+                                            class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>      
         </div>
