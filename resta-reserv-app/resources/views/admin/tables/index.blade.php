2b90b623bb7c49a3e674a4a010bf99fc0b92dece
kantm <kantm@DESKTOP-H26FR5O>
2023-11-01 18:06:11 +0100

    Add new mobile view of the tables table

--- resta-reserv-app/resources/views/admin/tables/index.blade.php
+++ resta-reserv-app/resources/views/admin/tables/index.blade.php
@@ -11,9 +11,10 @@
                 <a href="{{ route('admin.tables.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">New Table</a>
             </div>
             <div class="flex flex-col">
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg hidden sm:block">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-800 dark:bg-gray-8
+                        00 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
                                     Name
@@ -28,7 +29,7 @@
                                     Location
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Price
+                                    Action
                                 </th>
                                 
                             </tr>
@@ -67,6 +68,37 @@
                             
                         </tbody>
                     </table>
+                </div>
+                <!-- Mobile view table -->
+                <div class="grid grid-cols-1 gap-4 md:hidden sm:hidden">
+                    @foreach ($tables as $table)
+                    <div class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 rounded-lg shadow">
+                        <div class="space-x-3 text-xs text-gray-100 space-y-2">
+                            <div class="pl-3 pt-2 text-white uppercase text-sm">
+                                {{ $table->name }} - {{$table->guest_number}} seats - {{$table->status->name}}
+                            </div>
+                            <div class="flex justify-between">
+                                <div class="pr-12 bottom-0">
+                                    Location: {{ $table->location->name }}
+                                </div>
+                                <div>
+                                    <div class="flex space-x-2 pr-2 pb-2">
+                                        <a href="{{ route('admin.tables.edit', $table->id) }}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                                        <form
+                                            method="POST"
+                                            action="{{ route('admin.tables.destroy', $table->id) }}"
+                                            onsubmit="return confirm('Are you sure?');"
+                                            class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>      
         </div>
